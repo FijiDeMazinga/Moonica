b3ea68849b1a1c121c3e97fc4a9b3427dbaf4e97
FijiDeMazinga <fijidemazzinga@gmail.com>
2019-01-28 17:53:22 +0100

    login

--- app/src/main/java/com/moonica/fdm/Login.java
+++ app/src/main/java/com/moonica/fdm/Login.java
@@ -2,6 +2,7 @@ package com.moonica.fdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
     }
 }
