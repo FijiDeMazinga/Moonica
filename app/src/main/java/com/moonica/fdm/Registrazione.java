8a0157bee8a368eac47af188b2741b89eb0264d8
FijiDeMazinga <fijidemazzinga@gmail.com>
2019-01-29 12:26:05 +0100

    registrazione fixato

--- app/src/main/java/com/moonica/fdm/Registrazione.java
+++ app/src/main/java/com/moonica/fdm/Registrazione.java
@@ -3,6 +3,7 @@ package com.moonica.fdm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Registrazione extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class Registrazione extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione);
+        Log.d("prova", "prova");
         Intent registrazione = getIntent();
     }
 }
