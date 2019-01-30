8c2f97c5e9a79327c72f6d38b6da1fdddbf4a783
FijiDeMazinga <fijidemazzinga@gmail.com>
2019-01-30 18:48:49 +0100

    Minca la home crusha

--- app/src/main/java/com/moonica/fdm/Home.java
+++ app/src/main/java/com/moonica/fdm/Home.java
@@ -3,14 +3,21 @@ package com.moonica.fdm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
     TextView welcome;
     Utente u;
     String username;
+    LinearLayout l;
+    ArrayList<Corso> lista = new ArrayList<Corso>();
+    FactoryCorsi fc = FactoryCorsi.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,15 @@ public class Home extends AppCompatActivity {
         u = (Utente) obj;
         username = u.getUsername();
         welcome.setText("Benvenuto " + username + "!");
+
+        l = findViewById(R.id.corsi);
+        lista = fc.listaCorsiFacolta(u.getFacolta());
+
+        for(Corso c : lista){
+            Button b = new Button(this);
+            b.setText(c.getNome());
+            b.setBackgroundColor(225599);
+            l.addView(b);
+        }
     }
 }
