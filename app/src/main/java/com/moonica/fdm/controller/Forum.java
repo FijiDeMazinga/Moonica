4f8a07dca2e5447ad19050bf632f9ffb6a4b6847
C0ky <f.c.piredda@gmail.com>
2019-02-04 14:35:16 +0100

    Modifiche view & controller Forum

--- app/src/main/java/com/moonica/fdm/controller/Forum.java
+++ app/src/main/java/com/moonica/fdm/controller/Forum.java
@@ -1,15 +1,44 @@
 package com.moonica.fdm.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Space;
+import android.widget.TextView;
 
 import com.moonica.fdm.R;
+import com.moonica.fdm.model.ForumThread;
+
+import java.util.ArrayList;
+
 
 public class Forum extends AppCompatActivity {
+
+    TextView titolo;
+    ArrayList<ForumThread> listaForum = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+
+        Intent i = getIntent();
+
+        titolo.findViewById(R.id.forumWelcome);
+        //titolo.setText("Forum di " + ft.getCorso().getNome());
+
+        for (ForumThread ft : listaForum){
+            Button bt = new Button(this);
+            Space s = new Space(this);
+
+            s.setMinimumHeight(50);
+
+            if(ft.getTitolo().length() <= 30)
+                bt.setText(ft.getTitolo());
+            else
+                bt.setText(ft.getTitolo().substring(0, 30) + "...");
+
+        }
     }
 }
