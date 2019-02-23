ba01c239acea446c436f56f88da5fe5707d91fb7
FijiDeMazinga <fijidemazzinga@gmail.com>
2019-02-23 10:40:24 +0100

    correzione crash all'accesso di un corso vuoto

--- app/src/main/java/com/moonica/fdm/model/SezioneAdapter.java
+++ app/src/main/java/com/moonica/fdm/model/SezioneAdapter.java
@@ -62,37 +62,39 @@ public class SezioneAdapter extends RecyclerView.Adapter<SezioneAdapter.SezioneV
         sezioneViewHolder.cv.setCardBackgroundColor(0xff225599);
         sezioneViewHolder.titoloSezione.setTextColor(0xffffffff);
         sezioneViewHolder.titoloSezione.setTextSize(18);
-        for (int j=0 ; j<lista.get(i).getContenuti().size(); j++) {
-            Space space = new Space(OttieniContesto.getAppContext());
-            space.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
-            ImageView icona = new ImageView(OttieniContesto.getAppContext());
-            icona.setImageResource(lista.get(i).getContenuti().get(j).getIdIcona());
-            TextView testo = new TextView(OttieniContesto.getAppContext());
-            testo.setText(lista.get(i).getContenuti().get(j).getTesto());
-            GradientDrawable border = new GradientDrawable();
+        if (lista != null) {
+            for (int j=0 ; j<lista.get(i).getContenuti().size(); j++) {
+                Space space = new Space(OttieniContesto.getAppContext());
+                space.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+                ImageView icona = new ImageView(OttieniContesto.getAppContext());
+                icona.setImageResource(lista.get(i).getContenuti().get(j).getIdIcona());
+                TextView testo = new TextView(OttieniContesto.getAppContext());
+                testo.setText(lista.get(i).getContenuti().get(j).getTesto());
+                GradientDrawable border = new GradientDrawable();
 
-            border.setColor(0xeeeeeeee);
-            border.setStroke(3, 0xFF225599);
-            border.setCornerRadius(20.1f);
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                sezioneViewHolder.vistaContenuti.setBackgroundDrawable(border);
-            } else {
-                sezioneViewHolder.vistaContenuti.setBackground(border);
+                border.setColor(0xeeeeeeee);
+                border.setStroke(3, 0xFF225599);
+                border.setCornerRadius(20.1f);
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    sezioneViewHolder.vistaContenuti.setBackgroundDrawable(border);
+                } else {
+                    sezioneViewHolder.vistaContenuti.setBackground(border);
+                }
+                icona.setLayoutParams(new LinearLayout.LayoutParams(70, 70));
+                testo.setTextColor(0xFF225599);
+                testo.setTextSize(18);
+                testo.setPadding(20, 0, 0, 0);
+                LinearLayout horizontal = new LinearLayout(OttieniContesto.getAppContext());
+                horizontal.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                horizontal.setOrientation(LinearLayout.HORIZONTAL);
+                horizontal.addView(icona);
+                horizontal.addView(testo);
+                horizontal.addView(space);
+                horizontal.setPadding(80, 20, 0, 10);
+                sezioneViewHolder.vistaContenuti.addView(horizontal);
+                sezioneViewHolder.vistaContenuti.setPadding(0, 10, 0, 20);
+                sezioneViewHolder.vistaContenuti.setVisibility(View.GONE);
             }
-            icona.setLayoutParams(new LinearLayout.LayoutParams(70, 70));
-            testo.setTextColor(0xFF225599);
-            testo.setTextSize(18);
-            testo.setPadding(20, 0, 0, 0);
-            LinearLayout horizontal = new LinearLayout(OttieniContesto.getAppContext());
-            horizontal.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            horizontal.setOrientation(LinearLayout.HORIZONTAL);
-            horizontal.addView(icona);
-            horizontal.addView(testo);
-            horizontal.addView(space);
-            horizontal.setPadding(80, 20, 0, 10 );
-            sezioneViewHolder.vistaContenuti.addView(horizontal);
-            sezioneViewHolder.vistaContenuti.setPadding(0,10,0,20);
-            sezioneViewHolder.vistaContenuti.setVisibility(View.GONE);
         }
 
         sezioneViewHolder.attivatore.setOnClickListener(new View.OnClickListener() {
@@ -122,7 +124,10 @@ public class SezioneAdapter extends RecyclerView.Adapter<SezioneAdapter.SezioneV
 
     @Override
     public int getItemCount() {
-        return lista.size();
+        if (lista == null)
+            return 0;
+        else
+            return lista.size();
     }
 
     public static void expand(final View v) {
